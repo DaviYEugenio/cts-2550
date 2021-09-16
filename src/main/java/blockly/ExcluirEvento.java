2f4416d67e8e40ab493f2624eec8756a686bbdd9
Davi Yamane Eugenio <davi.yamane@cronapp.io>
2021-09-16 21:06:26 +0000

    c

--- src/main/java/blockly/ExcluirEvento.java
+++ src/main/java/blockly/ExcluirEvento.java
@@ -20,15 +20,17 @@ public static final int TIMEOUT = 300;
 public static Var ExcluindoEvento() throws Exception {
  return new Callable<Var>() {
 
-   private Var listaEventos = Var.VAR_NULL;
+   private Var evento = Var.VAR_NULL;
 
    public Var call() throws Exception {
 
-    listaEventos =
-    cronapi.list.Operations.newList(
-    cronapi.database.Operations.query(Var.valueOf("app.entity.Agenda"),Var.valueOf("select a from Agenda a where a.id IS NOT NULL")));
+    evento =
+    cronapi.screen.Operations.getValueOfField(
+    Var.valueOf("vars.excluirevento"));
 
-    System.out.println(listaEventos.getObjectAsString());
+    cronapi.util.Operations.callClientFunction( Var.valueOf("cronapi.screen.notify"), Var.valueOf("success"), evento);
+
+    cronapi.database.Operations.execute(Var.valueOf("app.entity.Agenda"), Var.valueOf("delete from Agenda where titulo = :titulo"),Var.valueOf("titulo",evento));
     return Var.VAR_NULL;
    }
  }.call();
